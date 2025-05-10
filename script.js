daf2f85e42cde7b2cde30939e4d9e91f7b11b0b3
Redgerandom <Regman163@gmail.com>
2025-05-09 20:27:06 -0400

    made operation function (+, - , * etc)

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+
+const add = (a, b) => a + b;
+const minus = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => b !== 0 ? a / b : 'Error: Division by zero';
+
+
+
+console.log(
+  add(15, 9)
+  , add(15, -9), add(15, 0)
+);
+
